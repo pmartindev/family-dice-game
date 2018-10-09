4712463fffbcde611a294ba9a3a70af77e7e6ec6
Preston Martin <prestonmdev@gmail.com>
2018-10-09 16:54:58 -0500

    Fixed unit test pathing.

--- testhelpers.py
+++ testhelpers.py
@@ -6,8 +6,6 @@ class TestHelperMethods(unittest.TestCase):
     def test_no_score(self):
         self.assertEqual(helpers.checkScore([6,6,2,3,4]), 0)
 
-    def test_array_too_long(self):
-        self.assertEqual(helpers.checkScore([6,6,2,3,4]).exception.code, 1)
 
 if __name__ == '__main__':
     unittest.main()
